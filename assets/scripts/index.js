c2bb92c1b41608cf830d0d9e4ff4f2d08e2661be
Mike MacDougall <mgmacdougall@outlook.com>
2025-02-20 12:37:01 -0500

    Add of support for button clicks and determining category, price and name

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,33 @@
-var addToCartEventListener = (e)=>
-{
-    // console.log("clicked", e)
-    console.log('clicked')
+var addToCartEventListener = (e) => {
+    console.log('clicked', e.target.id);
+    console.log('here')
+    var itemClicked = e.target.id.split('-')[0];
+    var categorySelector = `${itemClicked}-cat`;
+    var nameSelector = `${itemClicked}-name`;
+    var priceSelector = `${itemClicked}-price`;
+    var categoryText = document.getElementById(categorySelector).innerText;
+    var nameText = document.getElementById(nameSelector).innerText;
+    var priceText = document.getElementById(priceSelector).innerText;
+    console.log(categoryText, nameText, priceText)
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    document.addEventListener('click', e=>{
-        if(e.target.type ==="submit"){
-            console.log('add item to cart', e.target.id)
+document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener('click', e => {
+        var buttonNode = e.target;
+        if (e.target.type === "submit") {
+            addToCartEventListener(e);
         }
     })
 })
 
 var render = (data) => {
     data.forEach(element => {
-        console.log(element)
         const { name, image, price } = element;
         var container = document.getElementById('meal-items-container');
         const { desktop } = image;
-        console.log(desktop)
+        const itemName = name.replace(/[","," "]/g, '').toLowerCase();
         var mealCard = `
-        <div id="${name.replace(/[","," "]/g, '').toLowerCase()}" class="meal-card">
+        <div id="${itemName}" class="meal-card">
         <div class="meal-details">
         <div class="img-container">
         <img class="meal-image" src="${desktop}" alt="Image of waffle">
@@ -28,9 +35,9 @@ var render = (data) => {
         </div>
         <div>
         <ul class="meal-info">
-        <li class="meal-list-item li-none font-weight-light font-color-gray red-hat-text-200">${name.split(' ')[0]}</li>
-        <li class="meal-list-item li-none red-hat-text-500">${name}</li>
-        <li class="meal-list-item li-none red">${price}</li>
+        <li id="${itemName}-cat" class="meal-list-item li-none font-weight-light font-color-gray red-hat-text-200">${name.split(' ')[0]}</li>
+        <li id="${itemName}-name" class="meal-list-item li-none red-hat-text-500">${name}</li>
+        <li id="${itemName}-price" class="meal-list-item li-none red">${price}</li>
         </ul>
         </div>
         </div>
