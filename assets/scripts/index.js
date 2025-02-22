d28dda15ddce94f76564ddc9f221307abf847a36
Mike MacDougall <mgmacdougall@outlook.com>
2025-02-22 07:40:05 -0500

    Add functionality for add/subtract order count to button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,14 @@
 
-var updateCart = () => {
+var updateCart = (orderItem) => {
+
+
 
 
 }
 
 var displayOrderButton = e => {
     console.log(e.target.id.split('-').shift());
-    var baseId=e.target.id.split('-').shift();
+    var baseId = e.target.id.split('-').shift();
 
     var orderButton = document.getElementById(`${baseId}-button`);
     orderButton.classList.toggle('hidden');
@@ -29,10 +31,25 @@ var addToCartEventListener = (e) => {
 
 document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener('click', e => {
-        if (e.target.type === "submit") {
+
+        if (e.target.type === "submit" && !e.target.classList.contains('order-quantity-button')) {
             displayOrderButton(e);
             addToCartEventListener(e);
         }
+
+        console.log(e.target)
+        if (e.target.alt === "Remove") {
+            var orderCount = document.querySelector(`#${e.target.id}`).nextElementSibling;
+            var currentCount =parseInt(orderCount.innerText);
+            currentCount<=0? orderCount.innerText=0: orderCount.innerText-=1;
+        }
+
+        if (e.target.alt === "Add") {
+            var orderCount = document.querySelector(`#${e.target.id}`).previousElementSibling;
+            var currentCount =parseInt(orderCount.innerText);
+            orderCount.innerText=currentCount+=1;
+        }
+
     })
 })
 
@@ -48,7 +65,11 @@ var render = (data) => {
         <div class="img-container">
         <img class="meal-image" src="${desktop}" alt="Image of waffle">
         <button id="${name.replace(/[","," "]/g, '').toLowerCase()}-button" class="red-hat-text-400"><img src="/assets/images/icon-add-to-cart.svg" /><span>Add to card</span></button>
-        <button id="${name.replace(/[","," "]/g, '').toLowerCase()}-button-order" class="red-hat-text-400 order-quantity-button hidden"><img id="${name.replace(/[","," "]/g, '').toLowerCase()}-remove-item" src="/assets/images/icon-decrement-quantity.svg"/><span>0</span><img id="${name.replace(/[","," "]/g, '').toLowerCase()}-add-item" src="/assets/images/icon-increment-quantity.svg"></button>
+        <button id="${name.replace(/[","," "]/g, '').toLowerCase()}-button-order" class="red-hat-text-400 order-quantity-button hidden">
+            <img id="${name.replace(/[","," "]/g, '').toLowerCase()}-remove-item" src="/assets/images/icon-decrement-quantity.svg" alt="Remove"/>
+            <span>0</span>
+            <img id="${name.replace(/[","," "]/g, '').toLowerCase()}-add-item" src="/assets/images/icon-increment-quantity.svg" alt="Add">
+            </button>
         </div>
         <div>
         <ul class="meal-info">
